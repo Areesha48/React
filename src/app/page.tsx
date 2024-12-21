5e05b41c5639e80b2e76948d36e3528eda1142be
Areesha48 <areeshashehzad48@gmail.com>
2024-12-21 16:05:53 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Tasks from "./Tasks";
+export default function Home() {
+  // const name: string = "Iqra"
+  const data = 'loremdcxnb ui  hiuwd saxoi wdsaxuy8 ieqswyuax  yeq8idy'
+  return (
+    <div>
+      <Header name="Hello Button" color='red'/>
+      <Tasks data={data}/>
+    </div>
+  );
+  }
